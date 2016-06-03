725b1c6b66523af61a9ca5a861d874da273c606d
Alex Vance <alexdvance@gmail.com>
2016-06-03 16:45:48 -0400

    Changed directory to download dotfiles

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo 'You might need to change your default shell to zsh: `chsh -s /bin/zsh` (or `sudo vim /etc/passwd`)'
 
-dir="$HOME/Development/alexdvance"
+dir="$HOME/Development"
 mkdir -p $dir
 cd $dir
 git clone --recursive git://github.com/alexdvance/dotfiles.git
